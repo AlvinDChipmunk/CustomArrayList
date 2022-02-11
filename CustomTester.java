9c39b747f229bf0e0563fbe0a6e3b179b942a178
Alvin Revilas <alvinrevilas@yahoo.com>
2022-02-11 15:00:07 -0800

    Some installation notes
    Just in case, the comments at the top of this main file should help run all three files' lines of code smoothly, because that's how things are arranged on my machine for this Java project.

--- CustomTester.java
+++ CustomTester.java
@@ -1,3 +1,28 @@
+/*
+
+<-----------------------------< Project installation notes!! >----------------------------->
+
+This assignment project needs to be run in the following manner:
+
+1. In one project, the com.coderscampus.assignment5 package and the 
+com.coderscampus.assignment5.customarraylist package are inside the same source folder 
+(probably named "src").
+
+2. The two files, CustomList.java and CustomArrayList.java, are in the 
+com.coderscampus.assignment5.customarraylist package.
+
+3. The main testing file, CustomTester.java, is in the com.coderscampus.assignment5 package.
+
+4.  If these packages and associated files are in their proper places, this should be able 
+to run and execute the testing code.
+
+5.  Possible debugging idea:  Make a new project and make inside the src folder if using 
+Eclipse two packages, as named above.  Inside each package, create the appropriately named 
+files or file, and copy and paste the source code from here in GitHub into the destination 
+of those three newly created files correspondingly.
+
+*/
+
 package com.coderscampus.assignment5;
 
 import com.coderscampus.assignment5.customarraylist.*;
